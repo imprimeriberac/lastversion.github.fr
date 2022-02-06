bf03af12f379db690935df1f5471fc106a21929a
imprimeriberac <88195101+imprimeriberac@users.noreply.github.com>
2022-02-06 14:59:48 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/////////// TOGGLE MENU
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active')
+    menu.classList.toggle('active')
+}
